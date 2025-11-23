17058fef0a8843ecbb012ac4529ad40605875a2d
youni20 <moyounus20@gmail.com>
2025-11-23 13:31:50 +0100

    Initial Commit

---
+++ src/main/java/com/backend/lnuais_backend/LnuaisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.lnuais_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnuaisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnuaisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/lnuais_backend/LnuaisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lnuais_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnuaisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
