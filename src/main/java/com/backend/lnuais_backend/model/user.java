15ddb23e36ffc651896a6cd19cdc750d19c5a446
youni20 <moyounus20@gmail.com>
2025-11-23 15:05:46 +0100

    Implemented User Model

--- src/main/java/com/backend/lnuais_backend/model/user.java
+++ src/main/java/com/backend/lnuais_backend/model/user.java
@@ -1,5 +1,28 @@
 package com.backend.lnuais_backend.model;
 
-public class user{
+import lombok.Data;
+
+//@Entity
+@Data
+public class User{
+    int id;
+    String name;
+    String email;
+    String program;
     
+    public enum Experience{
+        LOW,
+        MID,
+        HIGH
+    };
+
+    Experience level;
+
+    public User(int id, String name, String email, String program, Experience level){
+       this.id = id;
+       this.name = name;
+       this.email = email;
+       this.program = program; 
+       this.level = level;
+    }   
 }
